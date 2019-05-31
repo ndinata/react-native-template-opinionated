decb86389c6c5aaa753d5af66dcfe7b2a4766a98
Nico Dinata <ncdnt@icloud.com>
2019-05-31 22:25:45 +1000

    Add summary note for linking dependencies

--- helper/summary.sh
+++ helper/summary.sh
@@ -2,7 +2,7 @@
 
 echo -e "React Native project \"$1\" succesfully created in ${BOLD}$PROJECT_DIR${NC}!"
 echo
-echo -e "$ARROW ${BOLD}LOG FILES${NC}"
+echo -e "$ARROW ${BOLD}Log Files${NC}"
 echo "Several log files were created in the project directory during the installation process. Feel free to remove them. These are:"
 if [ -f "$NPM_RN_CLI_LOGFILE" ]; then
     echo "- \`$NPM_RN_CLI_LOGFILE\` ——  when installing React Native CLI"
@@ -11,9 +11,15 @@ echo "- \`$RN_INIT_LOGFILE\` —— when initialising React Native project"
 echo "- \`$YARN_LOGFILE\` and \`$YARN_DEV_LOGFILE\` —— when installing packages with \`yarn\`"
 echo "- \`$PRETTIER_LOGFILE\` —— when prettifying .js files in project directory"
 echo
-echo -e "$ARROW ${BOLD}NOTE${NC}"
+echo -e "$ARROW ${BOLD}Installing Flow${NC}"
 echo "Because of .flowconfig's variable required version of \`flow-bin\`, this script opts to NOT install it automatically. Instead, you can do so yourself by checking the version number at the bottom of \$PROJECT_DIR/.flowconfig and running:"
 echo
 echo "    \`yarn add --dev flow-bin@x\`"
 echo
 echo "where \`x\` is the specified version."
+echo
+echo -e "$ARROW ${BOLD}Linking Dependencies${NC}"
+echo "In order to complete the installation of several dependencies, follow the instructions in the following links:"
+echo "    - https://reactnavigation.org/docs/en/getting-started.html"
+echo "    - https://realm.io/docs/javascript/latest/#installation"
+echo
