33a8e5e275566836cf213406b178e42fd7f6f1a3
Nico Dinata <ncdnt@icloud.com>
2019-09-28 21:09:02 +1000

    Update template inconsistencies

--- template/src/App.js
+++ template/src/App.js
@@ -1,7 +1,5 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
+ * MyProject
  * @format
  * @flow
  */
@@ -31,7 +29,8 @@ const App: () => React$Node = () => {
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+          style={styles.scrollView}
+        >
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
