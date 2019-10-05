10bfebfab0a9af55ea0c8f06ac1ab407d700a441
Nico Dinata <ncdnt@icloud.com>
2019-10-05 10:19:00 +1000

    Fix missing Reactotron configuration

--- template/index.js
+++ template/index.js
@@ -6,4 +6,9 @@ import { AppRegistry } from 'react-native';
 import App from './src/App';
 import { name as appName } from './app.json';
 
+// eslint-disable-next-line
+if (__DEV__) {
+  import('./ReactotronConfig');
+}
+
 AppRegistry.registerComponent(appName, () => App);
