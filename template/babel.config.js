b6421c7d29c3b0f92cf29fab7a16fb03f9257fe7
Nico Dinata <nicodnt@icloud.com>
2020-09-25 18:01:25 +1000

    [pkg] babel-plugin-transform-remove-console

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
 };
