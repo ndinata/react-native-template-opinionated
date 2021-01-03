03e456dbd88e206b712047db56a2b9497274a9a8
Nico Dinata <nicodnt@icloud.com>
2021-01-03 11:09:39 +1100

    Remove Flow

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset', '@babel/preset-flow'],
+  presets: ['module:metro-react-native-babel-preset'],
   env: {
     production: {
       plugins: ['transform-remove-console'],
