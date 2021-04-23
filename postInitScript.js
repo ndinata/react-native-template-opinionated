a015f7ae651ec26c88bbe262502cfbd908a87767
Savin Vadim <savinvadim1312@gmail.com>
2021-04-23 09:20:47 +0100

    Update postInitScript.js
    Without double quotes, the script fails when the path contains spaces

--- postInitScript.js
+++ postInitScript.js
@@ -4,4 +4,4 @@ const { execSync } = require('child_process');
 
 // initialise git repo for the project for Husky to work
 const projectPath = process.cwd();
-execSync(`git init ${projectPath}`);
+execSync(`git init "${projectPath}"`);
